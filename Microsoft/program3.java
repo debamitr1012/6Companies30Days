40be59258222ee15118c18cd18b9c86f3e1607e5
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2023-01-04 00:46:05 +0530

    Bulls and Cows

---
+++ Microsoft/program3.java
@@ -0,0 +1,21 @@
+class program3 {
+    public String getHint(String secret, String guess) {
+        int n=secret.length();
+        int[] s=new int[10];
+        int[] g=new int[10];
+        int bulls=0,cows=0;
+        for(int i=0;i<n;i++)
+        {
+            if(secret.charAt(i)==guess.charAt(i))
+                bulls++;
+            else
+            {
+                s[secret.charAt(i)-'0']++;
+                g[guess.charAt(i)-'0']++;
+            }
+        }
+        for(int i=0;i<10;i++)
+            cows+=Math.min(s[i],g[i]);
+        return (bulls+"A"+cows+"B");
+    }
+}
