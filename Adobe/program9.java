57c5005803504f6a16a86d5ed80830dcdd83110e
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2023-03-02 17:58:09 +0530

    Count Nodes Equal to Average of Subtree

---
+++ Adobe/program9.java
@@ -0,0 +1,28 @@
+public class Pair{
+    int f,s;
+    Pair(int f, int s){
+        this.f = f;
+        this.s = s;
+    }
+}
+class Solution {
+    int count;
+    public int averageOfSubtree(TreeNode root) {
+        count = 0;
+        isAverageBT(root);
+        return count;
+    }
+    public Pair isAverageBT(TreeNode root){
+        if(root==null){
+            return new Pair(0,0);
+        }
+        Pair left = isAverageBT(root.left);
+        Pair right = isAverageBT(root.right);
+        int sum = root.val + left.f + right.f;
+        int number = 1 + left.s + right.s;
+        if(sum/number == root.val){
+            count++;
+        } 
+        return new Pair(sum,number);
+    }
+}
